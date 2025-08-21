e432e61c99220069053c19730fa53a1d79d5c829
Ankur Manna <suchitrak5plus@gmail.com>
2025-08-21 23:35:32 +0530

    domain added

--- why/main.py
+++ why/main.py
@@ -1,4 +1,4 @@
-from .reasoner import invoke_reasoner_agent
+from reasoner import invoke_reasoner_agent
 import os
 import sys
 sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
@@ -7,8 +7,11 @@ from db import mutual_funds_collection
 mf_query_template = """
 Here is the details of a mutual fund that is chosen by financial advisor:
 {mutual_fund}
-Your job is to tell me WHY this mutual fund is chosen for the user using all the detials provided.
-Give 3-4 points explanantion on why this mutual fund might be chosen by the advisor.(Don't make it too long, just 3-4 points)
+Your job is to tell me WHY this mutual fund is chosen for the user using all the detials provided in a SIMPLE language.
+Your target audience is a user who is not familiar with financial terms. So explain it in a way that is easy to understand.
+Do not use any financial jargon or complex terms. Do not directly use the terms sortino ratio, Sharpe ratio, etc, rather explain what they mean in simple terms.
+Give 3-4 points(->) explanantion on why this mutual fund might be chosen by the advisor.(Don't take any assumptions, just use the data provided in the mutual fund details)
+You must have the first two points for returns and risk inference, they are most important things to be explained.
 """
 
 def mutual_fund_reasoner_tool(mutual_fund_details):
@@ -18,7 +21,7 @@ def mutual_fund_reasoner_tool(mutual_fund_details):
     return response
 
 if __name__ == "__main__":
-    fund_name = "HDFC Large Cap Fund - Direct Plan"
+    fund_name = "Edelweiss Equity Savings Fund - Direct Plan"
     fund = mutual_funds_collection.find_one({"fund_name": fund_name})
     if not fund:
         print(f"Mutual fund '{fund_name}' not found.")
